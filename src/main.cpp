c675dfa502fdf51a3b0a1c65ca35e8d09eb44420
linklekhehe <emdamde1102@gmail.com>
2025-02-27 08:38:42 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <BH1750.h>
+#include <Arduino.h>
+BH1750 lightMeter;
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    
+    if (lightMeter.begin()) {
+        Serial.println("BH1750 initialized!");
+    } else {
+        Serial.println("BH1750 initialization failed!");
+    }
+}
+
+void loop() {
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light Intensity: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+    delay(1000); // Đọc mỗi giây
+}
